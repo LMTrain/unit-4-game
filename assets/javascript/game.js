92a3bac15e2e6be5e6f7a99543fdbff15f62c068
LM Train <layconmm@msn.com>
2019-05-01 00:11:25 -0500

    modify and added new line of codes to the html, css and js files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,118 +1,128 @@
-function initializeReset() {
-    computerPick = 0;
-    finalNumber = 0;
-    hexagonNumber = 0;
-    diamondNumber = 0;
-    octagonNumber = 0;
-    squareNumber = 0;
-
-    // $("#final-number, #computer-pick, #status").empty();
-}
-
+$(document).ready(function () {
+    
 var computerPick = 0;
 var finalNumber = 0;
 var hexagonNumber = 0;
 var diamondNumber = 0;
 var octagonNumber = 0;
 var squareNumber = 0;
+    function initializeReset() {
+    
+        finalNumber = 0;
+        
+        hexagonNumber = Math.floor(Math.random() * 13);
+        diamondNumber = Math.floor(Math.random() * 13);
+        octagonNumber = Math.floor(Math.random() * 13);
+        squareNumber = Math.floor(Math.random() * 13);
+        computerPick = Math.floor(Math.random() * 121);
+        $("#computer-pick").text(computerPick);
+        $("#final-number").text(finalNumber);
+        console.log("initializeReset");
 
-hexagonNumber = Math.floor(Math.random() * 13);
-diamondNumber = Math.floor(Math.random() * 13);
-octagonNumber = Math.floor(Math.random() * 13);
-squareNumber = Math.floor(Math.random() * 13);
-computerPick = Math.floor(Math.random() * 121);
-$("#computer-pick").text(computerPick);
+        $("#random-button-1").attr("value", hexagonNumber);
+        $("#random-button-2").attr("value", diamondNumber);
+        $("#random-button-3").attr("value", octagonNumber);
+        $("#random-button-4").attr("value", squareNumber);
+        // $("#final-number, #computer-pick, #status").empty();
+        $(".crystal-button").unbind().on("click", function () {
+            var numberValue = parseInt($(this).attr('value'));
+
+            console.log("hexagonNumber = " + hexagonNumber);
+            finalNumber = finalNumber + numberValue;
+            console.log("AFTER CLICK ---" + " hexagonNumber = " + hexagonNumber + "  diamondNumber = " + diamondNumber + "  octagonNumber = " + octagonNumber + "  squareNumber = " + squareNumber);
+            $("#final-number").text(finalNumber);
+
+            if (finalNumber === computerPick) {
+                $("#status").text("You Won!");
+                $(this).unbind().click(function(){
+                    initializeReset();
+                })
+            }
+            else if (finalNumber > computerPick) {
+                $("#status").text("You Lose!");
+                $(this).unbind().click(function(){
+                    initializeReset();
+                })
+            }
+
+
+    });
+}
+
+initializeReset();
 // var randomTwo = Math.floor(Math.random() * 10);
 console.log("hexagonNumber = " + hexagonNumber + "  diamondNumber = " + diamondNumber + "  octagonNumber = " + octagonNumber + "  squareNumber = " + squareNumber);
 console.log("computerPick = " + computerPick);
-$(document).ready(function () {
-
-    $("#random-button-1").on("click", function () {
 
 
-        console.log("hexagonNumber = " + hexagonNumber);
-        finalNumber = finalNumber + hexagonNumber;
-        console.log("AFTER CLICK ---" + " hexagonNumber = " + hexagonNumber + "  diamondNumber = " + diamondNumber + "  octagonNumber = " + octagonNumber + "  squareNumber = " + squareNumber);
-        $("#final-number").text(finalNumber);
-
-        if (finalNumber === computerPick) {
-            $("#status").text("You Won!");
-            initializeReset();
-        }
-        else if (finalNumber > computerPick) {
-            $("#status").text("You Lose!");
-            initializeReset();
-        }
-
-
-    });
+    
 });
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    $("#random-button-2").on("click", function () {
-
-
-        console.log("diamondNumber = " + diamondNumber);
-        finalNumber = finalNumber + diamondNumber;
-        console.log("AFTER CLICK ---" + " hexagonNumber = " + hexagonNumber + "  diamondNumber = " + diamondNumber + "  octagonNumber = " + octagonNumber + "  squareNumber = " + squareNumber);
-        $("#final-number").text(finalNumber);
-
-        if (finalNumber === computerPick) {
-            $("#status").text("You Won!");
-            initializeReset();
-        }
-        else if (finalNumber > computerPick) {
-            $("#status").text("You Lose!");
-            initializeReset();
-        }
-
-    });
-});
-
-$(document).ready(function () {
-
-    $("#random-button-3").on("click", function () {
+//     $("#random-button-2").on("click", function () {
 
 
-        console.log("octagonNumber = " + octagonNumber);
-        finalNumber = finalNumber + octagonNumber;
-        console.log("AFTER CLICK ---" + " hexagonNumber = " + hexagonNumber + "  diamondNumber = " + diamondNumber + "  octagonNumber = " + octagonNumber + "  squareNumber = " + squareNumber);
-        $("#final-number").text(finalNumber);
+//         console.log("diamondNumber = " + diamondNumber);
+//         finalNumber = finalNumber + diamondNumber;
+//         console.log("AFTER CLICK ---" + " hexagonNumber = " + hexagonNumber + "  diamondNumber = " + diamondNumber + "  octagonNumber = " + octagonNumber + "  squareNumber = " + squareNumber);
+//         $("#final-number").text(finalNumber);
 
-        if (finalNumber === computerPick) {
-            $("#status").text("You Won!");
-            initializeReset();
-        }
-        else if (finalNumber > computerPick) {
-            $("#status").text("You Lose!");
-            initializeReset();
-        }
+//         if (finalNumber === computerPick) {
+//             $("#status").text("You Won!");
+//             initializeReset();
+//         }
+//         else if (finalNumber > computerPick) {
+//             $("#status").text("You Lose!");
+//             initializeReset();
+//         }
 
-    });
-});
+//     });
+// });
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    $("#random-button-4").on("click", function () {
+//     $("#random-button-3").on("click", function () {
 
 
-        console.log("squareNumber = " + squareNumber);
-        finalNumber = finalNumber + squareNumber;
-        console.log("AFTER CLICK ---" + " hexagonNumber = " + hexagonNumber + "  diamondNumber = " + diamondNumber + "  octagonNumber = " + octagonNumber + "  squareNumber = " + squareNumber);
-        $("#final-number").text(finalNumber);
+//         console.log("octagonNumber = " + octagonNumber);
+//         finalNumber = finalNumber + octagonNumber;
+//         console.log("AFTER CLICK ---" + " hexagonNumber = " + hexagonNumber + "  diamondNumber = " + diamondNumber + "  octagonNumber = " + octagonNumber + "  squareNumber = " + squareNumber);
+//         $("#final-number").text(finalNumber);
 
-        if (finalNumber === computerPick) {
-            $("#status").text("You Won!");
-            initializeReset();
-        }
-        else if (finalNumber > computerPick) {
-            $("#status").text("You Lose!");
-            initializeReset();
-        }
+//         if (finalNumber === computerPick) {
+//             $("#status").text("You Won!");
+//             initializeReset();
+//         }
+//         else if (finalNumber > computerPick) {
+//             $("#status").text("You Lose!");
+//             initializeReset();
+//         }
 
-    });
-});
+//     });
+// });
+
+// $(document).ready(function () {
+
+//     $("#random-button-4").on("click", function () {
+
+
+//         console.log("squareNumber = " + squareNumber);
+//         finalNumber = finalNumber + squareNumber;
+//         console.log("AFTER CLICK ---" + " hexagonNumber = " + hexagonNumber + "  diamondNumber = " + diamondNumber + "  octagonNumber = " + octagonNumber + "  squareNumber = " + squareNumber);
+//         $("#final-number").text(finalNumber);
+
+//         if (finalNumber === computerPick) {
+//             $("#status").text("You Won!");
+//             initializeReset();
+//         }
+//         else if (finalNumber > computerPick) {
+//             $("#status").text("You Lose!");
+//             initializeReset();
+//         }
+
+//     });
+// });
 
 
 
